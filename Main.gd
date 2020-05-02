935de5dbfd365a73b57df7065418398c4713be00
amalija-ramljak <amalija.ramljak@gmail.com>
2020-05-02 20:33:16 +0200

    Created mock levels, mock fields, and level opens on levelselector click

--- Main.gd
+++ Main.gd
@@ -1,6 +1,7 @@
 extends Control
 
 export (PackedScene) var LevelSelector
+var level_scene
 
 # prepare level list, all user data...
 func _ready():
@@ -15,13 +16,20 @@ func _ready():
 		level_selector.set_highscore(levels[level].highscore)
 		level_selector.set_progress(levels[level].full_runs, levels[level].current, levels[level].total)
 		level_selector.rect_position.y += i * 120
+		level_selector.connect("button_up", self, "on_level_selected", [level_selector.level_name])
 		if (!levels.playable.has(level)):
 			level_selector.disabled = true
 		i += 1
 
-func load(var path):
+func load(path):
 	var file = File.new()
 	file.open(path, File.READ)
 	var content = file.get_as_text()
 	file.close()
 	return content
+	
+func on_level_selected(level_name):
+	var path = "res://leveldata/scenes/%s.tscn" % level_name
+	self.level_scene = load(path)
+	print(level_name)
+	get_tree().change_scene_to(level_scene)
--- scenes/components/LevelSelector.gd
+++ scenes/components/LevelSelector.gd
@@ -1,12 +1,13 @@
 extends Button
 
-
+var level_name
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 func set_name(var name):
 	$LevelSelectorContainer/LevelInfoContainer/LevelNameLabel.text = " %s" % name
+	self.level_name = name
 
 func set_progress(var full_runs, var current, var total):
 	var progress_text = "          Prođena sva pitanja: %s\n          Trenutno odgovorena pitanja: %s/%s"
