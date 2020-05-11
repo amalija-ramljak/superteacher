17e23c9dacc6774062bc9d4898dab99b933e20a7
amalija-ramljak <amalija.ramljak@gmail.com>
2020-05-11 18:59:47 +0200

    filled save function

--- GameSaving.gd
+++ GameSaving.gd
@@ -2,6 +2,24 @@ func load_game():
 	var game = File.new()
 	game.open("user://game.save", File.READ)
 
-func save_game():
+func save_game(name, level_data):
 	var game = File.new()
-	game.open("user://game.save", File.WRITE)
+	game.open("user://game.save", File.READ_WRITE)
+	var data = parse_json(game.get_as_text())
+	# check for new unlocked level
+	var levels = File.new()
+	levels.open("res://leveldata/level_list.json")
+	var level_list = parse_json(levels.get_as_text())
+	var next_level = level_list.find(name) + 1
+	if next_level < level_list.size() and !data.unlocked.has(level_list[next_level]):
+		print("Calculate unlocking stuff rule")
+		var unlock_next = true
+		for i in range(next_level - 1):
+			unlock_next = unlock_next and data.levels[level_list[i]].full_passes > 0
+		var current_level = data.levels[name]
+		unlock_next = unlock_next and current_level.current_correct / current_level.total_pool >= 0.6
+		if unlock_next:
+			data.unlocked.append(level_list[next_level])
+	data.levels[name] = level_data
+	game.store_line(to_json(data))
+	game.close()
--- leveldata/LevelLoad.gd
+++ leveldata/LevelLoad.gd
@@ -7,7 +7,7 @@ func load_level(level_node, situation_indices, question_path):
 	set_situations(situation_indices, level_node)
 	return load_questions(question_path)
 
-func load_questions(question_path):
+func load_questions(_question_path):
 	return [{'question':'Text of question', 'answers': ['T', 'N1', 'N2', 'N3']}]
 
 func set_situations(indices, level_node):
