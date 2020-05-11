962671d5eb73c4f21f0859999646dd742b793488
amalija-ramljak <amalija.ramljak@gmail.com>
2020-05-11 20:55:37 +0200

    finished saving and loading functionalitites

--- GameSaving.gd
+++ GameSaving.gd
@@ -1,6 +1,28 @@
 func load_game():
 	var game = File.new()
+	var err = game.open("user://game.save", File.READ)
+	if err:
+		print("Nothing saved, so everything is empty!")
+		return empty_save()
+	else:
+		# contains the saved data, so basically only unlocked levels
+		var data = parse_json(game.get_as_text())
+		game.close()
+		var f = File.new()
+		f.open("res://leveldata/level_list.json", File.READ)
+		var levels = parse_json(f.get_as_text())
+		f.close()
+		for level in levels.list:
+			if !data.unlocked.has(level):
+				data.levels[level] = empty_level(level, levels.pool_size[level])
+		return data
+
+func load_level(name):
+	var game = File.new()
 	game.open("user://game.save", File.READ)
+	var level_data = parse_json(game.get_as_text()).levels[name]
+	game.close()
+	return level_data
 
 func save_game(name, level_data):
 	var game = File.new()
@@ -8,18 +30,64 @@ func save_game(name, level_data):
 	var data = parse_json(game.get_as_text())
 	# check for new unlocked level
 	var levels = File.new()
-	levels.open("res://leveldata/level_list.json")
-	var level_list = parse_json(levels.get_as_text())
-	var next_level = level_list.find(name) + 1
-	if next_level < level_list.size() and !data.unlocked.has(level_list[next_level]):
+	levels.open("res://leveldata/level_list.json", File.READ)
+	var level_list = parse_json(levels.get_as_text)
+	levels.close()
+	var next_level_index = level_list.list.find(name) + 1
+	var next_level_name = level_list.list[next_level_index]
+	if next_level_index < level_list.list.size() and !data.unlocked.has(next_level_name):
 		print("Calculate unlocking stuff rule")
 		var unlock_next = true
-		for i in range(next_level - 1):
-			unlock_next = unlock_next and data.levels[level_list[i]].full_passes > 0
+		for i in range(next_level_index - 1):
+			unlock_next = unlock_next and data.levels[level_list.list[i]].full_passes > 0
 		var current_level = data.levels[name]
-		unlock_next = unlock_next and current_level.current_correct / current_level.pool_size >= 0.6
+		unlock_next = unlock_next and current_level.current_correct / level_list.pool_size[name] >= 0.6
 		if unlock_next:
-			data.unlocked.append(level_list[next_level])
+			data.unlocked.append(next_level_name)	
 	data.levels[name] = level_data
+	var f = File.new()
+	var f_path = "res://leveldata/questions/%s.json" % next_level_name
+	f.open(f_path, File.READ)
+	data.levels[next_level_name] = {
+		'full_passes': 0,
+		'current_correct': 0,
+		'remaining_pool': parse_json(f.get_as_text()).keys(),
+		'highscore': 0,
+	}
+	f.close()
 	game.store_line(to_json(data))
 	game.close()
+
+# used for LevelSelector on first play
+func empty_save():
+	var f = File.new()
+	f.open("res://leveldata/level_list.json", File.READ)
+	var levels = parse_json(f.get_as_text())
+	f.close()
+	var save_data = {
+		'unlocked': ['Dvoriste'],
+		'levels': {}
+	}
+	for level in levels.list:
+		save_data.levels[level] = empty_level(level, levels.pool_size[level])
+	return save_data
+	
+# used for empty saves (first play?) and LevelSelector for locked levels
+func empty_level(name, pool_size):
+	var remaining_pool
+	# the first level cannot be entirely empty!
+	if name == 'Dvoriste':
+		# fetch pool from wherever
+		var f = File.new()
+		f.open("res://leveldata/questions/Dvoriste.json", File.READ)
+		remaining_pool = parse_json(f.get_as_text()).keys()
+		f.close()
+	else:
+		remaining_pool = []
+	return {
+		'full_passes': 0,
+		'pool_size': pool_size,
+		'current_correct': 0,
+		'remaining_pool': remaining_pool,
+		'highscore': 0,
+	}
