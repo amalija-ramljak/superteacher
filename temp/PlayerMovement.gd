49302d77ba202852b2db189c5ec5121efbef6cc7
amalija-ramljak <amalija.ramljak@gmail.com>
2020-05-11 21:54:29 +0200

    fixed a typo bug

--- temp/PlayerMovement.gd
+++ temp/PlayerMovement.gd
@@ -1,6 +1,6 @@
 static func move_player(avatar_node, fields, current_index, move_by):
 	var new_index = current_index + move_by
-	if new_index < field.size():
+	if new_index < fields.size():
 		avatar_node.rect_position = fields[new_index].rect_position
 	else:
 		# level finished
