280e11b24e20841e138fe91782364d1775f94705
amalija-ramljak <amalija.ramljak@gmail.com>
2020-05-11 21:03:34 +0200

    the LevelSelector data is loaded from the save data

--- scenes/VBoxContainer.gd
+++ scenes/VBoxContainer.gd
@@ -1,28 +1,23 @@
 extends VBoxContainer
 
-
 export (PackedScene) var LevelSelector
 
+var game_loader = preload("res://GameSaving.gd")
+
 func _ready():
-	var levels = self.load("res://leveldata/level_list.json")
-	levels = JSON.parse(levels).result
-	print(levels)
-	var i = 0
+	var f = File.new()
+	f.open("res://leveldata/level_list.json")
+	var levels = parse_json(f.get_as_text())
+	f.close()
+	var levels_data = game_loader.load_game()
 	for level in levels.list:
 		var level_selector = LevelSelector.instance()
 		add_child(level_selector)
+		var level_data = levels_data.levels[level]
 		level_selector.set_name(level)
-		level_selector.set_highscore(levels[level].highscore)
-		level_selector.set_progress(levels[level].full_runs, levels[level].current, levels[level].total)
+		level_selector.set_highscore(level_data.highscore)
+		level_selector.set_progress(level_data.full_passes, level_data.current_correct, levels.pool_size[level])
 		# terrible but oh well
 		level_selector.get_child(0).connect("button_up", self.get_parent().get_parent(), "on_level_selected", [level_selector.level_name])
-		if (!levels.playable.has(level)):
+		if (!levels_data.unlocked.has(level)):
 			level_selector.disable()
-		i += 1
-
-func load(path):
-	var file = File.new()
-	file.open(path, File.READ)
-	var content = file.get_as_text()
-	file.close()
-	return content
