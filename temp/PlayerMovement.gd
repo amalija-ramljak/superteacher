107accbd3eb7c6520fa08606a7cf2b5f2c2d6df4
amalija-ramljak <amalija.ramljak@gmail.com>
2020-05-11 21:53:07 +0200

    worked out how to move the player most likely

---
+++ temp/PlayerMovement.gd
@@ -0,0 +1,7 @@
+static func move_player(avatar_node, fields, current_index, move_by):
+	var new_index = current_index + move_by
+	if new_index < field.size():
+		avatar_node.rect_position = fields[new_index].rect_position
+	else:
+		# level finished
+		pass
