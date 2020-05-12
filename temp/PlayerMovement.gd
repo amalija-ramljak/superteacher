4c27bf7da0055abcff356afacaa1dcb0b3683479
amalija-ramljak <amalija.ramljak@gmail.com>
2020-05-12 17:54:02 +0200

    player movement and sprite loading from configfile

--- temp/PlayerMovement.gd
+++
@@ -1,7 +0,0 @@
-static func move_player(avatar_node, fields, current_index, move_by):
-	var new_index = current_index + move_by
-	if new_index < fields.size():
-		avatar_node.rect_position = fields[new_index].rect_position
-	else:
-		# level finished
-		pass
